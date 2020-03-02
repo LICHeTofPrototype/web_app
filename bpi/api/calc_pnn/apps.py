9ec5bcd6e151158fc599a4395935e8cdb7b46650
ynakagawa <napire223@gmail.com>
2020-03-03 02:20:04 +0900

    removed bpi directory

--- bpi/api/calc_pnn/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class calc_pnnConfig(AppConfig):
-    name = 'calc_pnn'
--- bpi/api/calc_pnn/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from . import views
-
-urlpatterns = [
-    path('', views.HeartBeatAPI.as_view(), name='heart-beat'),
-    #path('<str:requestType>/', views.pnnResult, name='pnnReult'),    
-]
-urlpatterns = format_suffix_patterns(urlpatterns)
