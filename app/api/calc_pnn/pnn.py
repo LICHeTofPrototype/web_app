0befc7655e7edfda0043e37e077017e1ba1d3321
Yuki Nakagawa <nakagawa@LogbiinoMacBook-Air-2.local>
2020-03-15 21:26:48 +0900

    changed prominence of find peak

--- app/api/calc_pnn/pnn.py
+++ app/api/calc_pnn/pnn.py
@@ -6,7 +6,7 @@ from scipy.signal import find_peaks
 #               Find RRI          
 #*******************************************************
 def find_RRI(time, data):       
-    peaks, _ = find_peaks(data, distance=20, prominence=0.5)
+    peaks, _ = find_peaks(data, distance=20, prominence=0.45)
     print ("In pnn.py peaks = ", peaks)
     peak_time = np.array(time)[peaks] * 0.001
     print ("In pnn.py peak_time = ", peak_time)
