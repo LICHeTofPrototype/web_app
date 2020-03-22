070b253a94f1cd409cd8ff38bb53a71463d6b701
神崎知波 <aaja2252@gmail.com>
2020-03-22 19:24:17 +0900

    フォーム画面

--- app/config/settings.py
+++ app/config/settings.py
@@ -89,8 +89,8 @@ TEMPLATES = [
     },
 ]
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
