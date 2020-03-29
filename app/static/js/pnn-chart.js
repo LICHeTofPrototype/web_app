96f3b1be0a0eea75309f83e127486dea5faf36b2
神崎知波 <aaja2252@gmail.com>
2020-03-29 18:48:21 +0900

    データない時の対応

--- app/static/js/pnn-chart.js
+++ app/static/js/pnn-chart.js
@@ -1,37 +1,40 @@
 $(function(){
-    var pnn = $("#pnn").val().split(",");
-    var pnnArray = [];
-    var time = [];
-    pnn.forEach(function(val, index, array){
-        time.push(index * 10);
-        pnnArray.push(parseFloat(val.replace("[","").replace("]","")));
-    });
-    console.info(pnnArray);
-    var canvas = document.getElementById("chart");
-    new Chart(canvas, {
-        type: 'line',
-        data: {
-            labels: time,
-            datasets:[{
-                label: "pnn",
-                data: pnnArray,
-                // borderColor: "rgba(255,0,0,1)",
-                // backgroundColor: "rgba(0,0,0,0)"
-            }]
-        },
-        options:{
-            title:{
-                text: "PNN-DATA",
-                display: true
-            },
-            scales: {
-                yAxes:[{
-                    ticks:{
-                        suggestedMax: 1,
-                        suggestedMin: 0
-                    }
+    var pnn = $("#pnn").val();
+    if(pnn != undefined){
+        pnn = pnn.split(",");
+        var pnnArray = [];
+        var time = [];
+        pnn.forEach(function(val, index, array){
+            time.push(index * 10);
+            pnnArray.push(parseFloat(val.replace("[","").replace("]","")));
+        });
+        console.info(pnnArray);
+        var canvas = document.getElementById("chart");
+        new Chart(canvas, {
+            type: 'line',
+            data: {
+                labels: time,
+                datasets:[{
+                    label: "pnn",
+                    data: pnnArray,
+                    // borderColor: "rgba(255,0,0,1)",
+                    // backgroundColor: "rgba(0,0,0,0)"
                 }]
+            },
+            options:{
+                title:{
+                    text: "PNN-DATA",
+                    display: true
+                },
+                scales: {
+                    yAxes:[{
+                        ticks:{
+                            suggestedMax: 1,
+                            suggestedMin: 0
+                        }
+                    }]
+                }
             }
-        }
-    })
+        })    
+    }
 });
