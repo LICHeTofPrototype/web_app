3d1478d725968296f5b0581073f733ebebcdbcfd
神崎知波 <aaja2252@gmail.com>
2020-03-31 22:04:19 +0900

    データのリアルタイム取得と表示(細かいところはできてない)

--- app/static/js/pnn-chart.js
+++ app/static/js/pnn-chart.js
@@ -1,65 +1,105 @@
 $(function(){
-    var pnn = $("#pnn").val();
-    if(pnn != undefined){
-        pnn = pnn.split(",");
-        var pnnArray = [];
-        var time = [];
-        pnn.forEach(function(val, index, array){
-            time.push(index * 10);
-            pnnArray.push(parseFloat(val.replace("[","").replace("]","")));
-        });
-        var canvas = document.getElementById("chart").getContext("2d");
-        var chart = new Chart(canvas, {
-            type: 'line',
-            // data: {
-            //     labels: time,
-            //     datasets:[{
-            //         label: "pnn",
-            //         data: pnnArray,
-            //         // borderColor: "rgba(255,0,0,1)",
-            //         // backgroundColor: "rgba(0,0,0,0)"
-            //     }]
-            // },
-            data: {
-                datasets:[{
-                    data:[]
-                },{
-                    data:[]
+    // var pnn = $("#pnn").val();
+    // if(pnn != undefined){
+    //     pnn = pnn.split(",");
+    //     var pnnArray = [];
+    //     var time = [];
+    //     pnn.forEach(function(val, index, array){
+    //         time.push(index * 10);
+    //         pnnArray.push(parseFloat(val.replace("[","").replace("]","")));
+    //     });
+    // }
+    var count = 0;
+    var canvas = document.getElementById("chart").getContext("2d");
+    var chart = new Chart(canvas, {
+        type: 'line',
+        data: {
+            datasets:[{
+                label:"pnn",
+                data:[]
+            }]
+        },
+        options:{
+            title:{
+                text: "PNN-DATA",
+                display: true
+            },
+            scales: {
+                xAxes:[{
+                    type: "realtime",
+                    ticks: {
+                        autoSkip: true,
+                        maxTicksLimit: 15 //値の最大表示数
+                    }
+                }],
+                yAxes:[{
+                    ticks:{
+                        suggestedMax: 1,
+                        suggestedMin: 0
+                    }
                 }]
             },
-            options:{
-                title:{
-                    text: "PNN-DATA",
-                    display: true
-                },
-                scales: {
-                    xAxes:[{
-                        type: "realtime"
-                    }],
-                    yAxes:[{
-                        ticks:{
-                            suggestedMax: 1,
-                            suggestedMin: 0
-                        }
-                    }]
-                },
-                plugins:{
-                    streaming:{
-                        duration: 20000,    
-                        refresh: 1000,      
-                        delay: 1000,        
-                        frameRate: 30,      
-                        pause: false,       
-                        onRefresh: function(chart) {
-                            console.info("onRefresh");
+            plugins:{
+                streaming:{
+                    duration: 20000,    
+                    refresh: 1000,      
+                    delay: 2000,        
+                    frameRate: 30,      
+                    pause: false,       
+                    onRefresh: function(chart) {
+                        getData().then(response =>{
+                            var pnn = response[count] == undefined ? 0 : response[count].pnn_data
+                            // TODO 表示時間の指定
                             chart.data.datasets[0].data.push({
                                 x: Date.now(),//ここに取得したデータの時間を入れる
-                                y: Math.random()//ここに取得した値を入れる
+                                y: pnn//ここに取得した値を入れる
                             });
-                        }
+                            count++;
+                        }).catch(error =>{
+                            count++;
+                            console.info(error);
+                            return error;
+                        });
                     }
                 }
             }
-        })    
-    }
+        }
+    })    
+
 });
+
+// TODO 現時点では連続して取得する対象のデータを判断できない
+function getData(request_index){
+    var body = {
+        measurement_id: 13,
+        request_index: 0
+    };
+    const url = '/v1/api/get_data/pnn/';
+    return fetch(url, {
+        method: "post",
+        headers: {
+            "X-CSRFToken": getCookie("csrftoken"),
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    }).then(function(response){
+        return response.json();
+    });
+}
+
+function getCookie(name) {
+    var result = null;
+    var cookieName = name + '=';
+    var allcookies = document.cookie;
+    var position = allcookies.indexOf( cookieName );
+    if( position != -1 ) {
+      var startIndex = position + cookieName.length;
+      var endIndex = allcookies.indexOf( ';', startIndex );
+      if( endIndex == -1 ) {
+        endIndex = allcookies.length;
+      }
+      result = decodeURIComponent(allcookies.substring( startIndex, endIndex ));
+    }
+    return result;
+}
