9f3fe56731c2e26f30dfe66719a623aa5f81de83
napire223 <napire223@gmail.com>
2020-03-31 21:54:13 +0900

    GetBeatDataの削除

--- app/api/get_data/views.py
+++ app/api/get_data/views.py
@@ -30,23 +30,3 @@ class GetPnnAPI(APIView):
         serializer = PnnDataSerializer(pnn_data_obj, many=True)
         print (serializer.data)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-class GetBeatAPI(APIView):
-    permission_classes = (AllowAny,)
-
-    def info(self, msg):
-        logger = logging.getLogger("command")
-        logger.info(msg)
-
-    def post(self, request, format=None):
-        measurement_obj = Measurement.objects.get(
-            id = request.data["measurement_id"],
-            #user = request.user
-        )
-        beat_data_obj = BeatData.objects.filter(
-            measurement = measurement_obj,
-            id__gt = request.data["request_index"]
-        )
-        serializer = PnnDataSerializer(pnn_data_obj, many=True)
-        print (serializer.data)
-        return Response(serializer.data, status=status.HTTP_200_OK)
