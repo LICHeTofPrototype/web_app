0e7c3c0b77b926b9e8f0ba03d21413779d4544ff
神崎知波 <aaja2252@gmail.com>
2020-03-31 20:23:53 +0900

    グラフのライブ表示まで

--- app/static/js/pnn-chart.js
+++ app/static/js/pnn-chart.js
@@ -8,17 +8,23 @@ $(function(){
             time.push(index * 10);
             pnnArray.push(parseFloat(val.replace("[","").replace("]","")));
         });
-        console.info(pnnArray);
-        var canvas = document.getElementById("chart");
-        new Chart(canvas, {
+        var canvas = document.getElementById("chart").getContext("2d");
+        var chart = new Chart(canvas, {
             type: 'line',
+            // data: {
+            //     labels: time,
+            //     datasets:[{
+            //         label: "pnn",
+            //         data: pnnArray,
+            //         // borderColor: "rgba(255,0,0,1)",
+            //         // backgroundColor: "rgba(0,0,0,0)"
+            //     }]
+            // },
             data: {
-                labels: time,
                 datasets:[{
-                    label: "pnn",
-                    data: pnnArray,
-                    // borderColor: "rgba(255,0,0,1)",
-                    // backgroundColor: "rgba(0,0,0,0)"
+                    data:[]
+                },{
+                    data:[]
                 }]
             },
             options:{
@@ -27,12 +33,31 @@ $(function(){
                     display: true
                 },
                 scales: {
+                    xAxes:[{
+                        type: "realtime"
+                    }],
                     yAxes:[{
                         ticks:{
                             suggestedMax: 1,
                             suggestedMin: 0
                         }
                     }]
+                },
+                plugins:{
+                    streaming:{
+                        duration: 20000,    
+                        refresh: 1000,      
+                        delay: 1000,        
+                        frameRate: 30,      
+                        pause: false,       
+                        onRefresh: function(chart) {
+                            console.info("onRefresh");
+                            chart.data.datasets[0].data.push({
+                                x: Date.now(),//ここに取得したデータの時間を入れる
+                                y: Math.random()//ここに取得した値を入れる
+                            });
+                        }
+                    }
                 }
             }
         })    
